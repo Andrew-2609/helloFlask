d2cc5b02bd8bac104fd9972cb32ced070c16802b
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-10-31 19:51:46 -0300

    Feat: add simple exception handling for GET method

--- dev_api/app.py
+++ dev_api/app.py
@@ -16,11 +16,18 @@ developers = [
 ]
 
 
+# noinspection PyBroadException
 @app.route('/dev/<int:developer_id>', methods=["GET", "PUT", "DELETE"])
 def developer(developer_id):
     if request.method == "GET":
-        found_developer = developers[developer_id]
-        return jsonify(found_developer)
+        try:
+            response = developers[developer_id]
+        except IndexError:
+            response = {"status": "error", "message": "developer not found"}
+        except Exception:
+            error_message = "unknown error, please get in touch with the API administrator (33224456789)"
+            response = {"status": "error", "message": error_message}
+        return jsonify(response)
     elif request.method == "PUT":
         new_data = json.loads(request.data)
         developers[developer_id] = new_data
