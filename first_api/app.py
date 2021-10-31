3682e11fd4c20dfecc56ed14c262e1f186e97f0b
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-10-31 15:42:41 -0300

    Feat: add jsonify to project

--- first_api/app.py
+++ first_api/app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def person_api():
-    return ""
+    return jsonify({"name": "Andrew"})
 
 
 if __name__ == '__main__':
