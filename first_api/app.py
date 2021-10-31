5eae69a7467b9aec0f97879243eea85480d2aa45
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-10-31 16:56:46 -0300

    Feat: add route to make a sum with GET method

--- first_api/app.py
+++ first_api/app.py
@@ -8,5 +8,11 @@ def person_api(person_id):
     return jsonify({"id": person_id, "name": "Andrew"})
 
 
+@app.route('/sum/get/<int:first_value>/<int:second_value>')
+def sum_get(first_value, second_value):
+    total = first_value + second_value
+    return jsonify({"sum": total})
+
+
 if __name__ == '__main__':
     app.run(debug=True)
