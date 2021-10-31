aa6374ee2d23c9f8f3e976499008282a5d861143
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-10-31 15:32:40 -0300

    Feat: add route to hello_name

--- run.py
+++ run.py
@@ -8,5 +8,10 @@ def hello_world():
     return "Hello, World!"
 
 
+@app.route('/<name>')
+def hello_name(name):
+    return f"Hello, {name}."
+
+
 if __name__ == '__main__':
     app.run(debug=True)
