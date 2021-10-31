b1f6eff65818c1e30f3a2016222610ce3f9408d5
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-10-31 19:24:59 -0300

    Feat: add list of developers and return one by list id

--- dev_api/app.py
+++ dev_api/app.py
@@ -2,10 +2,22 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+developers = [
+    {
+        "name": "Andrew Monteiro",
+        "skills": ["Java", "Python"]
+    },
+    {
+        "name": "Leandro Lima",
+        "skills": ["Java", "JavaScript"]
+    }
+]
 
-@app.route('/dev')
-def developer():
-    return jsonify({"message": "Hello, World!"})
+
+@app.route('/dev/<int:developer_id>')
+def developer(developer_id):
+    found_developer = developers[developer_id]
+    return jsonify(found_developer)
 
 
 if __name__ == '__main__':
