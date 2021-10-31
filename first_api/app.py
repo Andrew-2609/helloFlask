896222b1a75338efcefa445cae78e2fa1be9dba8
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-10-31 15:54:26 -0300

    Feat: add simple implementation of jsonify with converters

--- first_api/app.py
+++ first_api/app.py
@@ -3,9 +3,9 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/')
-def person_api():
-    return jsonify({"name": "Andrew"})
+@app.route('/<int:person_id>')
+def person_api(person_id):
+    return jsonify({"id": person_id, "name": "Andrew"})
 
 
 if __name__ == '__main__':
